872b87df6b03986991f883e862fa6164caec1537
mahmudin muttaqin <claxmanx@gmail.com>
2020-01-27 16:36:11 +0700

    ubah dikit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Switch } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter, Switch } from "react-router-dom";
 
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 
-import history from './utils/history'
-import { store, persistor } from './store';
-
 import AdminRoute from "routes/Admin";
 import MainRoute from "routes/Main";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <Router history={history}>
-        <Switch>
-          <MainRoute />
-       		<AdminRoute />
-        </Switch>
-      </Router>
-    </PersistGate>
-  </Provider>,
+  <BrowserRouter>
+    <Switch>
+      <MainRoute />
+   		<AdminRoute />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
