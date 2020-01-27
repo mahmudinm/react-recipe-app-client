17130d07572134f6eef40fe9245b315fc7477c70
mahmudin muttaqin <claxmanx@gmail.com>
2020-01-27 13:06:05 +0700

    ubah hook component jadi class component pada admin layout

--- src/containers/layouts/Admin.jsx
+++ src/containers/layouts/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 // reactstrap components
 import { Container } from "reactstrap";
@@ -7,26 +7,36 @@ import AdminNavbar from "components/Navbars/AdminNavbar.jsx";
 import AdminFooter from "components/Footers/AdminFooter.jsx";
 import Sidebar from "components/Sidebar/Sidebar.jsx";
 
-const Admin = (props) => {
-  return (
-    <>
-      <Sidebar
-        logo={{
-          innerLink: "/admin/index",
-          imgSrc: require("assets/img/brand/argon-react.png"),
-          imgAlt: "..."
-        }}
-      />
-      <div className="main-content">
-        <AdminNavbar
+// const Admin = (props) => {
+class Admin extends Component {
+
+  componentDidUpdate(e) {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.mainContent.scrollTop = 0;
+  }
+
+  render() {
+    return (
+      <>
+        <Sidebar
+          logo={{
+            innerLink: "/admin/index",
+            imgSrc: require("assets/img/brand/argon-react.png"),
+            imgAlt: "..."
+          }}
         />
-        {props.children}
-        <Container fluid>
-          <AdminFooter />
-        </Container>
-      </div>
-    </>
-  );
+        <div className="main-content" ref="mainContent">
+          <AdminNavbar
+          /> 
+          {this.props.children}
+          <Container fluid>
+            <AdminFooter />
+          </Container>
+        </div>
+      </>   
+    );
+  }
   
 }
 
