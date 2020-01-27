2a5849692b14ab0351ab2f8a80abefc805ca050e
mahmudin muttaqin <claxmanx@gmail.com>
2020-01-27 13:12:32 +0700

    ubah sedikit route

--- src/index.js
+++ src/index.js
@@ -6,21 +6,13 @@ import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 
-import LayoutRoute from "components/Router/LayoutRoute";
 import AdminRoute from "routes/Admin";
 import MainRoute from "routes/Main";
-
-import MainLayout from "containers/layouts/Main"
-
-import LoginPage from "containers/page/auth/login";
-import RegisterPage from "containers/page/auth/register";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-   		<LayoutRoute exact path="/auth/login" layout={MainLayout} component={LoginPage} />
-   		<LayoutRoute exact path="/auth/register" layout={MainLayout} component={RegisterPage} />
-
+      <MainRoute />
    		<AdminRoute />
     </Switch>
   </BrowserRouter>,
--- src/routes/main.js
+++ src/routes/main.js
@@ -3,11 +3,13 @@ import { Switch } from 'react-router-dom';
 import LayoutRoute from "components/Router/LayoutRoute";
 import MainLayout from "containers/layouts/Main";
 import LoginPage from "containers/page/auth/login";
+import RegisterPage from "containers/page/auth/register";
 
 const MainRoute = () => {
   return (
     <Switch>
-      <LayoutRoute path="/auth/login" layout={MainLayout} component={LoginPage} />
+      <LayoutRoute exact path="/auth/login" layout={MainLayout} component={LoginPage} />
+      <LayoutRoute exact path="/auth/register" layout={MainLayout} component={RegisterPage} />
     </Switch>
   )
 }
