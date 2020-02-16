4b887636b20194017a4195b4a0cac65e3e8669c8
mahmudin muttaqin <claxmanx@gmail.com>
2020-02-16 20:44:07 +0700

    tambah list grid toggle

--- src/containers/page/recipe/list/index.js
+++ src/containers/page/recipe/list/index.js
@@ -5,11 +5,15 @@ import {
   getMoreRecipeRequest
 } from "store/modules/recipe/actions";
 import {
-  Card,
-  CardBody,
   Col,
   Media,
   Input,
+  Card,
+  CardBody,
+  CardImg, 
+  CardText, 
+  CardTitle, 
+  CardSubtitle,  
   FormGroup,
   InputGroup,
   InputGroupAddon,
@@ -25,7 +29,8 @@ const RecipeListPage = () => {
   const next_page_url = useSelector(state => state.homeRecipe.next_page_url);
   const [search, setSearch] = useState('');
   const [categoryFilter, setCategoryFilter] = useState([]);
-  const [hasMore, setHasMore] = useState(true);  
+  const [hasMore, setHasMore] = useState(true);
+  const [listGrid, setListGrid] = useState('List');
   let timer = null;
 
 
@@ -59,6 +64,18 @@ const RecipeListPage = () => {
     setCategoryFilter(categoryFilter.filter(item => item.id !== id ));
     const categoryId = `${name}-${id}`;
     document.getElementById(categoryId).checked = false;
+  }
+
+  const handleListGrid = (e, value) => {
+    if(value === 'List') {
+      setListGrid('List')
+      e.target.classList.toggle('active');
+      document.getElementById('grid-button').classList.toggle('active');
+    } else if (value === 'Grid') {
+      setListGrid('Grid');
+      e.target.classList.toggle('active');
+      document.getElementById('list-button').classList.toggle('active');
+    }
   }
 
   return (
@@ -97,6 +114,11 @@ const RecipeListPage = () => {
               </div>
             </div>
 
+            <div className="btn-group float-right" role="group" aria-label="Basic example">
+              <button type="button" className="btn btn-secondary active" id="list-button" onClick={(e) => handleListGrid(e, 'List')} >LIST</button>
+              <button type="button" className="btn btn-secondary" id="grid-button" onClick={(e) => handleListGrid(e, 'Grid')} >GRID</button>
+            </div>            
+
             <div className="my-3">
               {categoryFilter.map((item, key) => 
                 <span className="badge badge-primary" style={{ lineHeight: '1.8', paddingLeft: '10px', paddingRight: '10px', marginRight: '10px' }} key={key}>
@@ -108,6 +130,8 @@ const RecipeListPage = () => {
               )}
             </div>
 
+            {listGrid}
+
             <InfiniteScroll
               className="mt-3"
               dataLength={recipes.length}
@@ -115,25 +139,42 @@ const RecipeListPage = () => {
               hasMore={hasMore}
               loader={<h4>Loading...</h4>}
               endMessage={<h4>Yay! You have seen it all</h4>}
+              style={{ overflow: 'hidden' }}
             >
-              {recipes.map((recipe, key) => 
-                <Card key={key} className="mb-3">
-                  <CardBody>
-                    <Media>
-                      <Media left>
-                        <Media object style={{ width: '200px' }} src={`http://localhost:8000/image/${recipe.image}`} alt={recipe.image} />
-                      </Media>
-                      <Media body className="ml-4" >
-                        <Media>
-                          <h2 className="media-heading">{recipe.name}</h2>
-                        </Media>
-                        <b>Category : {recipe.category.name}</b>
-                        <p>{recipe.step}</p>
-                      </Media>
-                    </Media>
-                  </CardBody>
-                </Card>
-              )}
+              <div className="row">
+                {recipes.map((recipe, key) =>  {
+                  return listGrid === 'List' ?  
+                    <Col lg="12" md="12" key={key}>
+                      <Card className="mb-3">
+                        <CardBody>
+                          <Media>
+                            <Media left>
+                              <Media object style={{ width: '200px' }} src={`http://localhost:8000/image/${recipe.image}`} alt={recipe.image} />
+                            </Media>
+                            <Media body className="ml-4" >
+                              <Media>
+                                <h2 className="media-heading">{recipe.name}</h2>
+                              </Media>
+                              <b>Category : {recipe.category.name}</b>
+                              <p>{recipe.step}</p>
+                            </Media>
+                          </Media>
+                        </CardBody>
+                      </Card>   
+                    </Col> :
+                    <Col lg="6" md="6" key={key}>
+                      <Card className="mt-3">
+                        <CardImg top width="100%" src={`http://localhost:8000/image/${recipe.image}`} alt={recipe.image}/>
+                        <CardBody>
+                          <CardTitle>{recipe.name}</CardTitle>
+                          <CardSubtitle>Category : {recipe.category.name}</CardSubtitle>
+                          <CardText>{recipe.step}</CardText>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                })}
+              </div>
+              <div className="clearfix"></div>
             </InfiniteScroll>
 
           </CardBody>
